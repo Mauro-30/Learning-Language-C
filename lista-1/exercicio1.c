c100d9e0eaecdbf86cd7ebfb82a86edef3299a5c
_MauroJesus <mauro301002@gmail.com>
2023-03-31 22:13:43 +0100

    Primeiro exercício

--- lista-1/exercicio1.c
+++ lista-1/exercicio1.c
@@ -1,60 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-typedef enum {JANEIRO=1,FEVEREIRO,MARCO,ABRIL,MAIO,JUNHO,JULHO,AGOSTO,SETEMBRO,OUTUBRO,NOVEMBRO,DEZEMBRO} Mes;
+int main(int argc, char *argv[]){
 
-int ContaDias(int mes){
-	if(mes == FEVEREIRO) {
-		return 28;
-	} else if(mes == JANEIRO || mes == MARCO || mes == MAIO || mes == JULHO || mes == AGOSTO || mes == OUTUBRO || mes == DEZEMBRO) {
-		return 31;
-	}
-	 else {
-		return 30;
-	}
+  int base,altura;
+  int area,perimetro;
+  
+  printf("Digite o tamanho da base:\n");
+  scanf("%d",&base);
+  
+  printf("Digite o tamanho da altura:\n");
+  scanf("%d",&altura);
+  
+  area= (base * altura)/2;
+  perimetro = (base*2)+(altura*2);
+  
+  printf("A area do triangulo igual a %d : \n",area);
+  printf("O perimetro igual a %d : \n",perimetro);
+  
+return 0;
 }
 
-int MesValido(int mes){
-	return (mes >= JANEIRO) && (mes <= DEZEMBRO);
-}
-
-int DiaValido(int mes, int dia) {
-  	return (dia >= 1) && (dia <= ContaDias(mes));	
-}	
-
-int UltimoDiaDoMes(int mes, int dia){
-	return ContaDias(mes) == dia;
-}
-
-int FimDoAno(int mes, int dia) {
-	return (mes==DEZEMBRO) && (UltimoDiaDoMes(mes,dia));
-}
-
-int main()
-{
-   int dia,mes,ano=2023;
-   
-   printf("Insira o dia e o mes (dd-mm): ");
-   scanf("%2d-%2d",&dia,&mes);
-   
-   if(!MesValido(mes)){
-   	  puts("Mês invalido!\n");
-   	  return -1;
-   }
-   if(!DiaValido(mes,dia)){
-   	   puts("Dia invalido!\n");
-   	   return -1;
-   }
-   if(FimDoAno(mes,dia)){
-   	  dia = mes = 1;
-   	  ano++;
-   } else {
-   		if(UltimoDiaDoMes(mes,dia)){
-   	 	dia = 1;
-   	 	mes++;
-	} else {
-		dia++;
-	}
-   }
-   printf("\n%02d-%02d-%4d\n",dia,mes,ano);
-   return 0;
-}
