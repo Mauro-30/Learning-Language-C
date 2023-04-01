eeee4cf91c0b0b4a3aaf52bb7dc46907123953ee
_MauroJesus <72946392+Mauro-30@users.noreply.github.com>
2023-04-01 05:25:27 +0100

    Update exercicio2.c

--- lista-1/exercicio2.c
+++ lista-1/exercicio2.c
@@ -6,14 +6,16 @@ int main(int argc, char *argv[]){
 	int n1,n2;
 	float quociente, resto;
 	
-	printf("Digite o o primeiro número:\n");
+	printf("Digite o o primeiro nÃºmero:\n");
   	scanf("%d",&n1);
-  	printf("Digite o segundo número:\n");
-    scanf("%d",&n2);
-    quociente = n1/n2;
-    resto = n1%n2;
-    printf("O quociente é %f \n",quociente);
-    printf("O resto é %f \n",resto);
+  	printf("Digite o segundo nÃºmero:\n");
+	scanf("%d",&n2);
+	
+	quociente = n1/n2;
+	resto = n1%n2;
+	
+	printf("O quociente Ã© %f \n",quociente);
+	printf("O resto Ã© %f \n",resto);
  return 0;
 }
 
